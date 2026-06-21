c91c51502e55fc3f348e167943ff55892579ac28
ROSELI PRADO DA SILVA <145849997+profrose26@users.noreply.github.com>
2026-06-21 10:01:31 -0300

    muda tema

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ botoes.forEach(function (botao) {
     let curtiu = false;
     botao.addEventListener("click", botaoClicado);
 
-
     function botaoClicado() {
         console.log("fui clicado");
         let texto = botao.querySelector("span");
@@ -16,3 +15,17 @@ botoes.forEach(function (botao) {
         }
     }
 });
+const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+btn - tema - escuro.addEventListener("click", mudaTema);
+
+function mudaTema() {
+    const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+}
+
+
